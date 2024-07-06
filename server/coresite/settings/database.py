d12ca7fc8052ef045d6597c8157230d0ebb1a36d
Shamasulhaq Python Developer <ulhaqshams924@gmail.com>
2024-07-06 16:58:30 +0500

    updated paypal

--- server/coresite/settings/database.py
+++ server/coresite/settings/database.py
@@ -1,12 +1,20 @@
-from .environment import env
+# from .environment import env
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('PASSWORD'),
+#         'HOST': env('HOST'),
+#         'PORT': 5432,
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
+from .base import BASE_DIR
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': 5432,
-        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
