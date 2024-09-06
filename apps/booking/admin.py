a2949761bc436a5d9be6ce28e6649f1d654e1022
Shamasulhaq Python Developer <ulhaqshams924@gmail.com>
2024-09-06 11:42:59 +0500

    add company staffs

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Bookings, ClientFeedback, ServiceFeedback, Journals
 
 # Register your models here.
+
+admin.site.register(Bookings)
+admin.site.register(ClientFeedback)
+admin.site.register(ServiceFeedback)
+@admin.register(Journals)
+class JournalsAdmin(admin.ModelAdmin):
+    pass
