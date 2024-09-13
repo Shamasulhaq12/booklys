24947becc8d24a9f4aa334d897c4d58f147f1d97
Shamasulhaq Python Developer <shamsulhaq@beyonderissolutions.com>
2024-09-13 18:35:21 +0500

    asas

--- apps/payments_and_subscription/models/subscriptions.py
+++ apps/payments_and_subscription/models/subscriptions.py
@@ -5,6 +5,7 @@ from coresite.mixin import AbstractTimeStampModel
 class SubscriptionFeature(AbstractTimeStampModel):
     feature_name = models.CharField(max_length=255)
     feature_description = models.TextField(null=True, blank=True)
+    feature_status = models.BooleanField(default=True)
     subscription = models.ForeignKey('Subscription', on_delete=models.CASCADE, related_name='subscription_features', null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
@@ -20,6 +21,7 @@ class Subscription(AbstractTimeStampModel):
     subscription_name = models.CharField(max_length=255)
     subscription_price = models.DecimalField(max_digits=10, decimal_places=2)
     subscription_duration_days = models.IntegerField(default=10)
+    description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
